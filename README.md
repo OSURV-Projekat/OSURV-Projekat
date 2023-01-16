383c15b43b69c7d07bd606003040c15c3ff38859
mokyii <babic.ra52.2020@uns.ac.rs>
2023-01-16 22:39:38 +0100

    Final
    Made motion sensor more robust & added documentation

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # OSURV-Projekat
  Projekat za predmet Operativni sistemi za rad u realnom vremenu
+ 
+ Pavle Vasiljević RA207/2020
+ Milica Vojnović RA59/2020
+ Mihailo Babić RA52/2020
+ Julija Jelićanin RA62/2020
--- motion.py
+++ motion.py
@@ -1,33 +1,20 @@
 import RPi.GPIO as GPIO
-import time
-from requests import get
+import threading
+import netifaces
 import smtplib
+import time
 
+
+PIR_SENSOR = 4
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(4, GPIO.IN) # PIR sensor
-
-
-def checkSensors():
-    time.sleep(5)
-    while True:
-        # Check if there is any motion detected
-        statusPIR =  GPIO.input(4)
-        if statusPIR == 1:  
-            time.sleep(0.25)
-            statusPIR =  GPIO.input(4)
-            if statusPIR == 1:
-                print("Motion detected!")
-                sendEmail()
-                time.sleep(60) 
-        time.sleep(0.25) 
-
+GPIO.setup(PIR_SENSOR, GPIO.IN) # PIR sensor   
     
-def sendEmail():
+
+def SendEmail():
     # Set the email 
-    ip = get('https://api.ipify.org').content.decode('utf8')
-    subject = 'Kretanje detektovano'
-    body = 'Aktivniran je senzor pokreta, proveri kameru na streamu:\n' + 'https://' \
-           + ip + ':8080/index.html' + '\n\n\n' + 'Vreme: ' + time.strftime("%H:%M:%S", time.localtime())
+    ip = netifaces.ifaddresses('wlan0')[netifaces.AF_INET][0]['addr']
+    subject = 'Detektovano kretanje'
+    body = 'Aktivniran je senzor pokreta, proveri kameru na streamu:\n' + 'http://' + ip + ':8080/index.html' + '\n\n\n' + 'Vreme: ' + time.strftime("%H:%M:%S", time.localtime())
 
     # Set up the SMTP server
     server = smtplib.SMTP('smtp.gmail.com', 587)
@@ -42,5 +29,41 @@ def sendEmail():
 
     # Disconnect from the server
     server.quit()
+    time.sleep(15)
+   
+    
+
+timePeriod = 5 # Seconds
+timeActive = 0
+timeInactive = 0
+
+
+try:
+    time.sleep(2) # Sleeps for 2 seconds to stabilize the sensor
+    print("PIR Sensor (CTRL+C to exit)")
+    while True:
+        if GPIO.input(PIR_SENSOR):
+            timeActive += 0.5
+            print("Motion detected! Counter: {}".format(int(timeActive * 2)))
+            time.sleep(0.5) # Add a delay to avoid false positives
+        else:
+            timeInactive += 0.5
+            print("No motion! Counter: {}".format(int(timeInactive * 2)))
+            time.sleep(1)
             
-checkSensors()
+        if timePeriod <= timeActive + timeInactive:
+            print("Reseting activity times")
+            if timeActive >= 2: #Active at least 40% to avoid false positives
+                print
+                print("There is confirmed motion activity, sending email notification!")
+                print
+                SendEmail()
+            timeActive = 0
+            timeInactive = 0
+            
+                 
+            
+
+except KeyboardInterrupt:
+    print("Quit")
+    GPIO.cleanup()
